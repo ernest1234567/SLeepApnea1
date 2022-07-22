55622df4b75cfdd9d397b9610e491d4c93aa3109
Ernest <2003937F@student.tp.edu.sg>
2022-07-22 10:15:34 +0800

    pdf

---
+++ Client/FileUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+
+namespace SLeepApnea.Client
+{
+    public static class FileUtil
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+           => js.InvokeAsync<object>(
+               "saveAsFile",
+               filename,
+               Convert.ToBase64String(data));
+    }
+}
